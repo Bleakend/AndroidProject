87702af7268d93c90b7186cec4a1fe94c6090b5c
Bilal <bedelbi1221@gmail.com>
2025-05-21 23:55:55 +0300

    Created the logic of the quizz and now it works, next I need to implement the scoring

--- app/src/main/java/com/example/quizzapp_i438/Quiz.java
+++ app/src/main/java/com/example/quizzapp_i438/Quiz.java
@@ -1,13 +1,15 @@
 package com.example.quizzapp_i438;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -15,29 +17,29 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class Quiz extends AppCompatActivity {
 
+public class Quiz extends AppCompatActivity {
+    private Fragment fragment;
+    private FragmentContainerView fragment_container;
     private FirebaseFirestore database;
-    private TextView tv_question;
-    private TextView answer1;
-    private TextView answer2;
-    private TextView answer3;
+    public int question_index = 0;
+    public int correct_answers = 0;
+    public ArrayList<Question> question_list = new ArrayList<>();
+    public Question current_question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        fragment_container = findViewById(R.id.fragment_container);
 
         FirebaseApp.initializeApp(this);
         database = FirebaseFirestore.getInstance();
-
-        tv_question = (TextView) findViewById(R.id.label_question);
-        answer1 = (TextView) findViewById(R.id.label_answer1);
-        answer2 = (TextView) findViewById(R.id.label_answer2);
-        answer3 = (TextView) findViewById(R.id.label_answer3);
 
         database.collection("questions")
                 .get()
@@ -45,24 +47,20 @@ public class Quiz extends AppCompatActivity {
                     @Override
                     public void onSuccess(QuerySnapshot querySnapshot) {
                         List<DocumentSnapshot> question_documents = querySnapshot.getDocuments();
+                        Collections.shuffle(question_documents);
+                        for (int i = 0;i < 5;i++) {
+                            DocumentSnapshot random_question = question_documents.get(i);
 
-                        if (question_documents.isEmpty()) {
-                            return;
+                            String question_text = random_question.getString("question");
+                            String answer1_text = random_question.getString("answer1");
+                            String answer2_text = random_question.getString("answer2");
+                            String correct_answer_text = random_question.getString("correct_answer");
+                            String[] options = {answer1_text, answer2_text, correct_answer_text};
+                            question_list.add(new Question(question_text, options, correct_answer_text));
                         }
-                        int random_index = new Random().nextInt(question_documents.size());
-                        DocumentSnapshot random_question = question_documents.get(random_index);
-
-                        String question_text = random_question.getString("question");
-                        String answer1_text = random_question.getString("answer1");
-                        String answer2_text = random_question.getString("answer2");
-                        String correct_answer_text = random_question.getString("correct_answer");
-                        String[] options = {answer1_text, answer2_text, correct_answer_text};
-                        Question question = new Question(question_text, options, correct_answer_text);
-                        tv_question.setText(question.get_question());
-                        answer1.setText(question.get_options()[0]);
-                        answer2.setText(question.get_options()[1]);
-                        answer3.setText(question.get_options()[2]);
-
+                        current_question = question_list.get(0);
+                        ((QuizFragment) fragment).update_question(question_list.get(0));
+                        fragment_container.setVisibility(FragmentContainerView.VISIBLE);
                     }
                 });
     }
---
+++ app/src/main/java/com/example/quizzapp_i438/QuizFragment.java
@@ -0,0 +1,96 @@
+package com.example.quizzapp_i438;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class QuizFragment extends Fragment {
+    public QuizFragment() {
+        // Required empty public constructor
+    }
+
+    public static QuizFragment newInstance() {
+        return new QuizFragment();
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Quiz activity = (Quiz) getActivity();
+        
+        Button submit_button = view.findViewById(R.id.button_submit_answer);
+        RadioGroup radio_group = view.findViewById(R.id.answers_group);
+        TextView question_number_label = activity.findViewById(R.id.question_number_label);
+
+        submit_button.setOnClickListener(v -> {
+            int selected_id = radio_group.getCheckedRadioButtonId();
+            if (selected_id == -1){
+                Toast.makeText(activity, "Please select an answer before submitting", Toast.LENGTH_LONG).show();
+                return;
+            }
+            RadioButton selected_radio_button = view.findViewById(selected_id);
+            String selected_answer = selected_radio_button.getText().toString();
+            String correct_answer = activity.current_question.get_correct_answer();
+
+            if (selected_answer.equals(correct_answer)) {
+                activity.correct_answers += 1;
+            }
+
+            activity.question_index += 1;
+            question_number_label.setText(String.format("Question %s of 5", String.valueOf(activity.question_index + 1)));
+            if (activity.question_index == activity.question_list.size() - 1){
+                submit_button.setText(R.string.submit_quiz_label);
+            }
+
+            if (activity.question_index == activity.question_list.size()) {
+//                Intent intent = new Intent(activity, QuizResult.class);
+//                intent.putExtra("score", activity.correct_answers);
+//                startActivity(intent);
+            } else {
+                // Show next question
+                activity.current_question = activity.question_list.get(activity.question_index);
+                update_question(activity.current_question);
+                radio_group.clearCheck();
+            }
+
+        });
+    }
+
+    public void update_question(Question question) {
+        TextView questionLabel = getView().findViewById(R.id.label_question);
+        RadioButton answer1 = getView().findViewById(R.id.radio_answer1);
+        RadioButton answer2 = getView().findViewById(R.id.radio_answer2);
+        RadioButton answer3 = getView().findViewById(R.id.radio_answer3);
+
+        questionLabel.setText(question.get_question());
+
+        String[] options = question.get_options();
+        int start = new Random().nextInt(3);
+
+        answer1.setText(options[start % 3]);
+        answer2.setText(options[(start + 1) % 3]);
+        answer3.setText(options[(start + 2) % 3]);
+    }
+}
